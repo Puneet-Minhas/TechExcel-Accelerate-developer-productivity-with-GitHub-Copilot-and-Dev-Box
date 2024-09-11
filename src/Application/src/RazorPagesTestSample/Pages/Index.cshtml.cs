6c749d230aeb8a4cb707013c6f7ea01f5e002795
Admin user <you@example.com>
2024-09-11 13:36:29 +0100

    security updates

--- src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
+++ src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
@@ -94,14 +94,16 @@ namespace RazorPagesTestSample.Pages
 
      
         
- public static void WriteToDirectory(ZipArchiveEntry entry, string destDirectory)
- {
-     string destFileName = Path.GetFullPath(Path.Combine(destDirectory, entry.FullName));
-     string fullDestDirPath = Path.GetFullPath(destDirectory + Path.DirectorySeparatorChar);
-     if (!destFileName.StartsWith(fullDestDirPath)) {
-         throw new System.InvalidOperationException("Entry is outside the target dir: " + destFileName);
-     }
-     entry.ExtractToFile(destFileName);
- }
+        public static void WriteToDirectory(ZipArchiveEntry entry,
+                                            string destDirectory)
+        {
+            string destFileName = Path.GetFullPath(Path.Combine(destDirectory, entry.FullName));
+            string fullDestDirPath = Path.GetFullPath(destDirectory + Path.DirectorySeparatorChar);
+            if (!destFileName.StartsWith(fullDestDirPath)) {
+                throw new System.InvalidOperationException("Entry is outside the target dir: " +
+                                                                                    destFileName);
+            }
+            entry.ExtractToFile(destFileName);
+        }
     }
 }
